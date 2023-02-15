1db6f94b74b12dde7ed151400170545917376369
kimiji1986 <75070710@qq.com>
2023-02-15 20:25:54 +0800

    Update script.js

--- client/public/script.js
+++ client/public/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5137/', {
+    const response = await fetch('https://metacrowd-x.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
